c04283528fbf50a21d8294c69a678ba1596aedb8
Andres <andresrp645@gmail.com>
2023-11-07 17:56:38 -0600

    enviroment

--- src/cfg/keys.js
+++ src/cfg/keys.js
@@ -2,9 +2,9 @@ import 'dotenv/config';
 
 module.exports = {
     database: {
-        host: process.env.HOST || 'aws.connect.psdb.cloud', 
-        user: process.env.USER || '24pdskc067rprju2p2p9',
-        password: process.env.PASSWD || 'pscale_pw_5HCfyH9l8YRdJuHRUOUyZ6xTtdr25Wi1KtomXmtDNOB',
+        host: process.env.HOST || 'localhost', 
+        user: process.env.USER || 'root',
+        password: process.env.PASSWD || '',
         database: process.env.DB || 'impactoacademico',
         ssl:{
             rejectUnauthorized: true
