d7fd1daa4978bff0cf50ba01e4981d4666b09d35
Andres Rosales <59161981+AndresRP645@users.noreply.github.com>
2024-05-23 14:19:09 -0600

    Update keys.js
    Signed-off-by: Andres Rosales <59161981+AndresRP645@users.noreply.github.com>

--- src/cfg/keys.js
+++ src/cfg/keys.js
@@ -3,7 +3,7 @@ import 'dotenv/config';
 module.exports = {
     database: {
         host: process.env.HOST || 'localhost', 
-        user: process.env.USER || 'root',
+        user: process.env.USERDB || 'root',
         password: process.env.PASSWD || '',
         database: process.env.DB || 'impactoacademico',
         /*ssl:{
